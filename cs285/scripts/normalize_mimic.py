ca6f8af46bb46500fea3208d097ac5f98191465c
Ali Shirali <ali_9938@yahoo.com>
2023-02-07 19:54:54 -0800

    normalize mimic added.

---
+++ cs285/scripts/normalize_mimic.py
@@ -0,0 +1,61 @@
+import os
+import pickle
+
+import numpy as np
+from tqdm import tqdm
+
+from cs285.infrastructure import utils
+
+
+if __name__ == '__main__':
+    do_save = True
+
+    file_path = os.path.join('..', '..', 'Replay_buffer_extraction')
+    file_name = 'Encoded_paths13_all_rewards'
+
+    sparse_reward_tag = 'sparse_90d_rew'
+    gamma = 1
+
+    # Read data
+    with open(os.path.join(file_path, file_name + '.pkl'), 'rb') as f:
+        paths = pickle.load(f)
+
+    # Concat all paths
+    concat_rews = {}
+    for path in tqdm(paths):
+        path['Reward_pseudo'] = np.ones((len(path[sparse_reward_tag]),))
+        for key, val in path.items():
+            if key.startswith('Reward') or (key == sparse_reward_tag):
+                if key in concat_rews:
+                    concat_rews[key] = np.concatenate((concat_rews[key], utils.discounted_cumsum(val, gamma)))
+                else:
+                    concat_rews[key] = utils.discounted_cumsum(val, gamma)
+
+    # Calc mean and std
+    rew_mean = {}
+    rew_std = {}
+    for key, val in concat_rews.items():
+        rew_mean[key] = np.mean(val)
+        rew_std[key] = np.std(val)
+
+        print('%s: [%g, %g]' % (key, rew_mean[key], rew_std[key]))
+
+    # Var 1
+    normalized_paths = []
+    for path in tqdm(paths):
+        normalized_path = {}
+        for key, val in path.items():
+            if key.startswith('Reward'):
+                normalized_path[key] = val / rew_std[key]
+            elif key == sparse_reward_tag:
+                normalized_path[key] = val
+                normalized_path[key + '_n'] = val / rew_std[key]
+            else:
+                normalized_path[key] = val
+
+        normalized_paths.append(normalized_path)
+
+    # Save results
+    if do_save:
+        with open(os.path.join(file_path, file_name + '_var1.pkl'), 'wb') as f:
+            pickle.dump(normalized_paths, f)
